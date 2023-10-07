4bc721c7f5117a11ee0999bce20b94310998d560
Abdelabset <Abdelbaset.Abidi@studmail.w-hs.de>
2023-10-07 17:40:20 +0200

    Create User & UserRepository

--- CafeManagmentApp/src/main/java/com/cafe/CafeManagmentApp/model/User.java
+++ CafeManagmentApp/src/main/java/com/cafe/CafeManagmentApp/model/User.java
@@ -1,11 +1,44 @@
 package com.cafe.CafeManagmentApp.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
 
+@Data
+@Entity
+@Table(name = "user")
+@AllArgsConstructor
+@NoArgsConstructor
 public class User implements Serializable {
 
 
+    private final static long serialVersionUID=1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+
+    private String contactNumber;
+
+    private String email;
+
+    private String password;
+
+    private String status;
+
+    private String role;
 
 
-    
+
+
+
+
+
+
+
 }
---
+++ CafeManagmentApp/src/main/java/com/cafe/CafeManagmentApp/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.cafe.CafeManagmentApp.repository;
+
+import com.cafe.CafeManagmentApp.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Integer> {
+}
