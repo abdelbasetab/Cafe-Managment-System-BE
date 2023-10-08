1bb41f5d2d744314fade7c078c423f9018972388
AbdBaset <Abdelbaset.Abidi@studmail.w-hs.de>
2023-10-08 03:29:10 +0200

    SignUp Task

--- CafeManagmentApp/src/main/java/com/cafe/CafeManagmentApp/restController/UserController.java
+++ CafeManagmentApp/src/main/java/com/cafe/CafeManagmentApp/restController/UserController.java
@@ -7,7 +7,6 @@ import com.cafe.CafeManagmentApp.utils.CafeUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
