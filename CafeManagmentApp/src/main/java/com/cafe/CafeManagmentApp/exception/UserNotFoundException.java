ba8c986cd8e35da9ac83d16abdfc8ab9e45b5bcd
AbdBaset <Abdelbaset.Abidi@studmail.w-hs.de>
2023-10-08 05:40:09 +0200

    SignIn Task

---
+++ CafeManagmentApp/src/main/java/com/cafe/CafeManagmentApp/exception/UserNotFoundException.java
@@ -0,0 +1,8 @@
+package com.cafe.CafeManagmentApp.exception;
+
+public class UserNotFoundException extends Exception {
+
+    public UserNotFoundException(String msg){
+        super(msg);
+    }
+}
